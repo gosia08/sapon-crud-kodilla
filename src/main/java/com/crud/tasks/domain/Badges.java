75565a9f6e7ec11c22322c3c21991cec0368831d
gosia08 <sapon.gosia@gmail.com>
2019-05-20 13:11:33 +0200

    modul 18

---
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Badges {
+
+    private int votes;
+
+    @JsonProperty("attachmentsByType")
+    private AttachmentsByType attachments;
+
+}
